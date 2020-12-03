21603992e43d5044800af74d6c58353f3cc3f174
georgejob <45026762+georgejob@users.noreply.github.com>
2020-12-02 21:50:14 -0500

    Add files via upload

--- Moma.java
+++ Moma.java
@@ -4,15 +4,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Moma extends AppCompatActivity {
 
+    int[] ticketCount = {0, 1, 2, 3, 4, 5};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moma);
+
+        Toast.makeText(Moma.this, "Maximum of 5 tickets for each!", Toast.LENGTH_SHORT).show();
     }
 
     public void geWebsite(View view)
