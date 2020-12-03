611757fe17285c3255472591e5323f7698e33342
georgejob <45026762+georgejob@users.noreply.github.com>
2020-12-03 04:16:34 -0500

    Add files via upload

--- Moma.java
+++ Moma.java
@@ -5,12 +5,15 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Moma extends AppCompatActivity {
+public class Moma extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    int[] ticketCount = {0, 1, 2, 3, 4, 5};
+    String[] ticketCount = {"0", "1", "2", "3", "4", "5"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,32 @@ public class Moma extends AppCompatActivity {
         setContentView(R.layout.activity_moma);
 
         Toast.makeText(Moma.this, "Maximum of 5 tickets for each!", Toast.LENGTH_SHORT).show();
+
+        Spinner spin = (Spinner) findViewById(R.id.adultSpinner);
+        spin.setOnItemSelectedListener(this);
+
+        Spinner spin2 = (Spinner) findViewById(R.id.seniorSpinner);
+        spin.setOnItemSelectedListener(this);
+
+        Spinner spin3 = (Spinner) findViewById(R.id.childSpinner);
+        spin.setOnItemSelectedListener(this);
+
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,ticketCount);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(aa);
+        spin2.setAdapter(aa);
+        spin3.setAdapter(aa);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
+        //Toast.makeText(getApplicationContext(), ticketCount[position], Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+    // TODO Auto-generated method stub
+
     }
 
     public void geWebsite(View view)
